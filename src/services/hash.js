c4c54d3e1d724e183a3a6be446703c6f050b883a
hasnain-cedcoss <syedhasnainmehadi@cedcommerce.com>
2023-09-12 12:24:12 +0530

    password hashing

--- src/services/hash.js
+++ src/services/hash.js
@@ -5,7 +5,10 @@ import { promisify } from 'util';
 const scryptAsync = promisify(scrypt);
 
 export const hashPassword = async (pass) => {
+  // generate a salt
   const salt = randomBytes(16).toString('hex');
+
+  // encrypt the password
   const buf = await scryptAsync(pass, salt, 64);
   return `${buf.toString('hex')}.${salt}`;
 };
@@ -13,10 +16,13 @@ export const hashPassword = async (pass) => {
 export const verifyPassword = async (pass, hashPass) => {
   // split salt
   const [hashedPassword, salt] = hashPass.split('.');
+
   // we need to pass buffer values to timingSafeEqual
   const hashedPasswordBuf = Buffer.from(hashedPassword, 'hex');
+
   // we hash the new sign-in password
   const suppliedPasswordBuf = await scryptAsync(pass, salt, 64);
+
   // compare the new supplied password with the stored hashed password
   return timingSafeEqual(hashedPasswordBuf, suppliedPasswordBuf);
 };
