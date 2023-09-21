61950250687b2ce3fd87e3f004c4ae6e772f7bc1
hasnain-cedcoss <syedhasnainmehadi@cedcommerce.com>
2023-09-21 17:18:53 +0530

    remove unused code & packages

--- src/services/request.js
+++ src/services/request.js
@@ -1,8 +1,6 @@
-import axios from 'axios';
-
 export const reActive = async () => {
   try {
-    await axios.get('https://node-server-setup-2-0.onrender.com/');
+    await fetch('https://node-server-setup-2-0.onrender.com/');
   } catch (error) {
     console.log('error', error);
   }
